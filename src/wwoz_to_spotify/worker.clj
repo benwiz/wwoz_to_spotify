62c864d878c6129cff8567da9ca2896dba9f160d
Ben Wisialowski <bwisialowski@gmail.com>
2018-02-27 22:35:55 -0600

    call main function and return nil

--- src/wwoz_to_spotify/worker.clj
+++ src/wwoz_to_spotify/worker.clj
@@ -1,7 +1,15 @@
 (ns wwoz_to_spotify.worker)
 
+(defn wwoz-to-spotify
+  "Get playlist track list, consume RSS feed, identify each on Spotify,
+  if song is not in track list add to new list, add all tracks in new list
+  to playlist."
+  []
+  nil)
+
 (defn run
   "Start the whole thing."
   []
   (println "running...")
-  "Success!")
+  (wwoz-to-spotify)
+  nil)
