dabcf64f21bb6e81b2bb0a9de68cb3e66e8977ef
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-01 09:57:10 -0600

    description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject wwoz_to_spotify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Read WWOZ's Spinitron RSS feed and add all songs to a Spotify playlist."
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
