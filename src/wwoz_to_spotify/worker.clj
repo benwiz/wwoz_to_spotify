ec94f7237bb2bea74a60988010dc3cefac1e6c72
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-01 12:30:09 -0600

    auth issues but spotify is working

--- src/wwoz_to_spotify/worker.clj
+++ src/wwoz_to_spotify/worker.clj
@@ -1,6 +1,7 @@
 (ns wwoz_to_spotify.worker)
 (require 'feedme)
 (require '[clj-spotify.core :as spotify])
+(require '[clj-spotify.util :as spotify-util])
 
 (defn clean-feed
   "Clean the feed by replacing java.util.Date with strings.
@@ -19,13 +20,27 @@
   (let [feed (feedme/parse url)]
     (clean-feed feed)))
 
+(defn get-spotify-token
+  "Get OAuth2 token."
+  []
+  (spotify-util/get-access-token
+   (System/getenv "SPOTIFY_CLIENT_ID")
+   (System/getenv "SPOTIFY_CLIENT_SECRET")))
+
+(defn spotify-handler
+  "Do all Spotify stuff. Eventually will need to take track info from RSS feed."
+  [track_uri]
+  (println "Do Spotify stuff...")
+  (let [token (get-spotify-token)]
+    (println
+     (spotify/add-tracks-to-a-playlist {:user_id "bwisialowski" :playlist_id "3vjFwtIxnPkNXk0XWTj0wy" :uris [track_uri]} token)))
+  nil)
+
 (defn consume-wwoz-rss
   "Consume WWOZ's Spinitron RSS feed."
   []
   (println "Consume RSS feed...")
-  (println
-   (get
-    (read-feed "https://spinitron.com/public/rss.php?station=wwoz") :entries))
+  (get (read-feed "https://spinitron.com/public/rss.php?station=wwoz") :entries)
   nil)
 
 (defn wwoz-to-spotify
@@ -33,7 +48,8 @@
   if song is not in track list add to new list, add all tracks in new list
   to playlist."
   []
-  (consume-wwoz-rss)
+  ; (consume-wwoz-rss)
+  (spotify-handler "spotify:track:5nZ7hoYakhGtfEizQKA1fp")
   nil)
 
 (defn run
