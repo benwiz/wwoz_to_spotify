46b71a9fb44b878c681c46d498602660f407913c
Ben Wiz <bwisialowski@gmail.com>
2019-11-03 10:15:39 -0600

    prep to search spotify

--- src/wwoz_to_spotify/worker.clj
+++ src/wwoz_to_spotify/worker.clj
@@ -3,6 +3,7 @@
    [aleph.http :as http]
    [byte-streams :as bs]
    [cheshire.core :as cheshire]
+   [clojure.string :as str]
    [clj-spotify.core :as spotify]
    [feedme]
    [hickory.core :as h]
@@ -18,62 +19,6 @@
    :spotify-client-secret (System/getenv "SPOTIFY_CLIENT_SECRET")
    :spotify-refresh-token (System/getenv "SPOTIFY_REFRESH_TOKEN")
    :spotify-playlist-id   "3vjFwtIxnPkNXk0XWTj0wy"})
-
-(defn clean-feed
-  "Clean the feed by replacing java.util.Date with strings.
-   Clean envelope and each entry."
-  [feed]
-  (update (update feed :published str)
-          :entries (fn [entries]
-                     (map (fn [entry]
-                            (update entry :published str))
-                          entries))))
-
-(defn read-feed
-  "Parse the provided feed."
-  [url]
-  (let [feed (feedme/parse url)]
-    (clean-feed feed)))
-
-(defn get-spotify-token
-  "Get OAuth2 token."
-  []
-  (spotify-util/get-access-token
-   (System/getenv "SPOTIFY_CLIENT_ID")
-   (System/getenv "SPOTIFY_CLIENT_SECRET")
-   (System/getenv "SPOTIFY_REFRESH_TOKEN")))
-
-(defn get-recent-tracks-spotify
-  "Get `n` most recently added tracks from the playlist"
-  [user-id playlist-id n token]
-  (map
-   (fn [item] (get (get item :track) :uri))
-   (get
-    (spotify/get-a-playlists-tracks {:user_id user-id
-                                     :playlist_id playlist-id
-                                     :fields "items(track(uri))"
-                                     :limit 50
-                                     :offset 0}
-                                    token)
-    :items)))
-
-(defn search-spotify
-  "Search for song on Spotify, return URI."
-  [search-term token]
-  (let [search-term (clojure.string/replace (clojure.string/replace search-term "'" "") ":" "")]
-    (println "Search spotify for:" search-term)
-    (let [track-uri (get
-                     (get
-                      (get
-                       (get
-                        (spotify/search {:q search-term :type "track" :limit 1} token)
-                        :tracks)
-                       :items)
-                      0)
-                     :uri)]
-      (println "Track URI:" track-uri)
-      track-uri)))
-
 
 (defn consume-html
   "Consume WWOZ's Last ~7 songs Played HTML.
@@ -108,35 +53,63 @@
                           :album  (get d "r")})))))))
 
 
-(defn wwoz-to-spotify
-  "Get playlist track list, consume RSS feed, identify each on Spotify,
-  if song is not in track list add to new list, add all tracks in new list
-  to playlist."
+(defn spotify-token
+  "Get OAuth2 token."
   []
-  (let [user-id             (:spotify-user-id config)
-        playlist-id         (:spotify-playlist-id)
-        token               (get-spotify-token)
-        recently-added-uris (get-recent-tracks-spotify user-id playlist-id 50 token)]
-    (map (fn [entry]
-                                        ; Get track URI and if one was found and not recently
-                                        ; added to playlist then add it to the playlist.
-           (let [track-uri (search-spotify (get entry :title) token)]
-             (if track-uri
-               (if (not (some #{track-uri} recently-added-uris))
-                 #_(spotify/add-tracks-to-a-playlist {:user_id     user-id
-                                                      :playlist_id playlist-id
-                                                      :uris        [track-uri]
-                                                      :position    0}
-                                                     token)
-                 (println "Add track to playlist" {:user_id     user-id
-                                                   :playlist_id playlist-id
-                                                   :uris        [track-uri]
-                                                   :position    0})))))
-         (consume-wwoz-html))))
+  (spotify-util/get-access-token
+   (:spotify-client-id config)
+   (:spotify-client-secret config)
+   (:spotify-refresh-token config)))
+
+
+(defn search-spotify
+  "Search for song on Spotify, return URI."
+  [search-term token]
+  (prn "Search for:" search-term)
+  #_(let [search-term (str/replace (str/replace search-term "'" "") ":" "")]
+    (println "Search spotify for:" search-term)
+    (let [track-uri (-> (spotify/search {:q search-term :type "track" :limit 1} token)
+                        :tracks
+                        :items
+                        first
+                        :uri)]
+      (println "Track URI:" track-uri)
+      track-uri)))
+
+
+(defn spotify-tracks
+  "Get the Spotify track from the song and artist names."
+  [tracks token]
+  (map (fn [track]
+         (search-spotify (str (:song track) ", " (:artist track)) token))
+       tracks))
+
+
+(defn recent-spotify-tracks [tracks token]
+  {:new    tracks
+   :recent (map
+            #(get-in % [:track :uri])
+            (:items (spotify/get-a-playlists-tracks {:user_id     (:spotify-user-id config)
+                                                     :playlist_id (:spotify-playlist-id config)
+                                                     :fields      "items(track(uri))"
+                                                     :limit       (count tracks)
+                                                     :offset      0}
+                                                    token)))})
+
+
+
+
 
 (defn run
   "Start the whole thing."
   []
   (println "Run...")
-  (wwoz-to-spotify)
-  nil)
+  (let [token (spotify-token)]
+    (-> (consume-html)
+        parse-html
+        (spotify-tracks token)
+        #_recent-spotify-tracks
+        ;; TODO: Use `diff` to remove overlaps
+        ))
+  ;; TODO: What return value?
+  )
